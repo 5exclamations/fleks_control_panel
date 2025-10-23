4dc18052286eba45a0827b252bd3c4d235cb63cb
Tehran <teha00002@stud.uni-saarland.de>
2025-10-24 00:33:10 +0400

    added new allowed host for deployment

--- DjangoProject1/settings.py
+++ DjangoProject1/settings.py
@@ -27,7 +27,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'fleks-control-panel.onrender.com',
+    '127.0.0.1',
+    'localhost'
+]
 
 
 # Application definition
